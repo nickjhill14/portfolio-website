e5a12d7bf62e466692c648f7d2c82ec97b72121d
Nicholas J Hill <nicholasjhill@live.co.uk>
2022-03-29 16:49:40 +0100

    Add heading and fonts to Landing Page

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App, { headingLine1, headingLine2, headingLine3 } from './App';
+import App, { headingLines } from './App';
 
 describe('<App/>', () => {
     it('renders header', () => {
         render(<App />);
 
         expect(screen.getByRole('banner', { name: 'Landing Page Header' }));
-        expect(screen.getByRole('heading', { name: headingLine1 }));
-        expect(screen.getByRole('heading', { name: headingLine2 }));
-        expect(screen.getByRole('heading', { name: headingLine3 }));
+        headingLines.forEach((headingLine) =>
+            expect(screen.getByRole('heading', { name: headingLine }))
+        );
     });
 });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,17 @@ import './app.css';
 export default function App() {
     return (
         <header aria-label="Landing Page Header">
-            <h1>{headingLine1}</h1>
-            <h1>{headingLine2}</h1>
-            <h1>{headingLine3}</h1>
+            {headingLines.map((headingLine, index) => (
+                <h1 className="headingLine" key={`heading-line-${index}`}>
+                    {headingLine}
+                </h1>
+            ))}
         </header>
     );
 }
 
-export const headingLine1 =
-    "I'm Nicholas Hill, a Software Developer Consultant at Thoughtworks in London.";
-export const headingLine2 =
-    'I graduated with a Masters degree in Computer Science from the University of Nottingham.';
-export const headingLine3 =
-    "I've also worked and studied Mandarin Chinese in Shenzhen and Hong Kong.";
+export const headingLines = [
+    "I'm Nicholas Hill, a Software Developer Consultant at Thoughtworks in London.",
+    'I graduated with a Masters degree in Computer Science from the University of Nottingham.',
+    "I've also worked and studied Mandarin Chinese in Shenzhen and Hong Kong.",
+];
