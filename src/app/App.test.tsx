9c0e33f88f0bbfff725381ea152a466ab948b300
Nicholas J Hill <nicholasjhill@live.co.uk>
2022-03-29 16:28:49 +0100

    Add header to landing page

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { headingLine1, headingLine2, headingLine3 } from './App';
 
 describe('<App/>', () => {
-    it('renders title', () => {
+    it('renders header', () => {
         render(<App />);
-        expect(
-            screen.getByRole('heading', { name: 'Portfolio Website' })
-        ).toBeInTheDocument();
+
+        expect(screen.getByRole('banner', { name: 'Landing Page Header' }));
+        expect(screen.getByRole('heading', { name: headingLine1 }));
+        expect(screen.getByRole('heading', { name: headingLine2 }));
+        expect(screen.getByRole('heading', { name: headingLine3 }));
     });
 });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,5 +2,18 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-    return <h1>Portfolio Website</h1>;
+    return (
+        <header aria-label="Landing Page Header">
+            <h1>{headingLine1}</h1>
+            <h1>{headingLine2}</h1>
+            <h1>{headingLine3}</h1>
+        </header>
+    );
 }
+
+export const headingLine1 =
+    "I'm Nicholas Hill, a Software Developer Consultant at Thoughtworks in London.";
+export const headingLine2 =
+    'I graduated with a Masters degree in Computer Science from the University of Nottingham.';
+export const headingLine3 =
+    "I've also worked and studied Mandarin Chinese in Shenzhen and Hong Kong.";
